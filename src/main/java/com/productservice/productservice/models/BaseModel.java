bdf32451605b7c8714b1fd2cd1c29f54b8256db8
sahil <sahilramwal99@gmail.com>
2023-11-30 02:39:15 +0530

    Connected with database

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -1,5 +1,16 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+@MappedSuperclass
+@Setter
+@Getter
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
